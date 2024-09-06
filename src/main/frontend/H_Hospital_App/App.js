2b22fca954f2a19219162aed5af9ce38090b89df
dlfjs4585 <dlfjs4585@gmail.com>
2024-09-06 12:28:54 +0900

    09-06 start

--- src/main/frontend/H_Hospital_App/App.js
+++ src/main/frontend/H_Hospital_App/App.js
@@ -4,7 +4,6 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <header>이건 헤더입니다.</header>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
