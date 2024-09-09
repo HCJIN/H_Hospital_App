781ba2a46aee0cdd1784272574d4133614816727
dlfjs4585 <dlfjs4585@gmail.com>
2024-09-09 09:31:38 +0900

    mapper, vo update

--- src/main/java/com/green/H_Hospital_App/member/vo/MemberVO.java
+++ src/main/java/com/green/H_Hospital_App/member/vo/MemberVO.java
@@ -1,6 +1,13 @@
 package com.green.H_Hospital_App.member.vo;
 
+import lombok.Data;
+
+@Data
 public class MemberVO {
-
-
+    private int memNum;
+    private String email;
+    private String memPw;
+    private String memName;
+    private String memTel;
+    private String memRole;
 }
