7cfe3a281fdad35eafd33b79ed9a7ef95cf3a06e
dlfjs4585 <dlfjs4585@gmail.com>
2024-09-11 14:50:16 +0900

    error update

--- src/main/java/com/green/H_Hospital_App/member/service/MemberService.java
+++ src/main/java/com/green/H_Hospital_App/member/service/MemberService.java
@@ -9,4 +9,6 @@ public interface MemberService {
     // 회원가입시 데이터를 받아오는 메서드
     void insertMember(MemberVO memberVO);
 
+    // 위치 업데이트 메서드 추가
+    void updateLocation(String email, double latitude, double longitude);
 }
